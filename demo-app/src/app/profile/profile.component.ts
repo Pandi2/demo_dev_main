c61ac47a7336f2975f0eb60660c4f33720eb69b8
Aravinthan S <aravinthan@embien.com>
2024-07-01 08:48:05 +0530

    Intial Commit

--- demo-app/src/app/profile/profile.component.ts
+++ demo-app/src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap, Route } from '@angular/router';
+import { CharService } from '../char.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+
+  userData:any
+
+  constructor(private route:ActivatedRoute,private charService:CharService){
+
+  }
+  
+
+  ngOnInit(){
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      console.log(params.get('id'))
+      var id:any = params.get('id')
+      this.charService.getcharctorsDetails(id).subscribe((result:any)=>{
+       this.userData = result
+        console.log(result)
+      })
+
+  })
+  }
 
 }
